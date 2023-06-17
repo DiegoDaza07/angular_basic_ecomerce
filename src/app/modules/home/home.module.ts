379a15b881ca15944bb7843834d1a3226b0c3708
diegodaza07 <diegodaza07@gmail.com>
2023-06-17 19:33:58 -0300

    error import

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,8 +3,8 @@ import { CommonModule, NgFor } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { FooterComponent, ProductCardComponent, ToolbarComponent } from 'src/app/components';
-import { CarouselImgsComponent } from './components';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { CarouselImgsComponent } from './components/carousel-imgs';
 
 
 @NgModule({
